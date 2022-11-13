01619c4e3601e5a98ece90de2ce8407dd71b99a4
MD ASIF KABIR EMON <mdasifkabiremon@MDs-MacBook-Air.local>
2022-11-13 09:23:22 +0800

    search and sorting functionality added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,13 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ServiceCard from "../ServiceCard/ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [isAsc, setIsAsc] = useState(true);
+  const searchRef = useRef();
+  const [search, setSearch] = useState("");
   useEffect(() => {
-    fetch("https://genius-car-server-beige.vercel.app/services")
+    fetch(
+      `https://genius-car-server-beige.vercel.app/services?search=${search}&order=${
+        isAsc ? "asc" : "desc"
+      }`
+    )
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [isAsc, search]);
+
+  const handleSearch = () => {
+    setSearch(searchRef.current.value);
+  };
 
   return (
     <div className="my-10">
@@ -19,9 +30,26 @@ const Services = () => {
           humour, or randomised <br /> words which don't look even slightly
           believable.
         </p>
+        <div className="my-5">
+          <input
+            type="text"
+            ref={searchRef}
+            placeholder="Type here"
+            className="input input-sm input-bordered input-primary w-full max-w-xs"
+          />
+          <button onClick={handleSearch} className="btn btn-sm mx-1">
+            Search
+          </button>
+        </div>
+        <div className="my-5 text-xl font-bold">
+          <span>Sorting:</span>
+          <button onClick={() => setIsAsc(!isAsc)} className="btn btn-sm mx-3">
+            {isAsc ? "desc" : "asc"}
+          </button>
+        </div>
       </div>
       <div className="flex justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
           {services.map((service) => (
             <ServiceCard key={service.id} service={service}></ServiceCard>
           ))}
@@ -29,7 +57,13 @@ const Services = () => {
       </div>
 
       <div className="text-center">
-        <button className="btn btn-outline btn-error">More Services</button>
+        {services.length > 0 ? (
+          <button className="btn btn-outline btn-error mt-10">
+            More Services
+          </button>
+        ) : (
+          <p className="text-xl">Nothing Found from Your Keyword Searching</p>
+        )}
       </div>
     </div>
   );
